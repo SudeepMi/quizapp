87af92fc4f1131f495acdddb713ecb75313acb9b
sudeepmi <sudeepm0599@gmail.com>
2022-11-10 21:53:25 +0545

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# contest aaplication
+This application provides interface for quiz contest event.
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+  return (
+    <>
+    <div className='question'>
+      <h2>Which data structure uses FIFO techniques?</h2>
+      <div className='option_area'>
+        <p>A. Stack</p>
+        <p>B. Queue</p>
+        <p>C. Circular Queue</p>
+        <p>D. Linked List</p>
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
