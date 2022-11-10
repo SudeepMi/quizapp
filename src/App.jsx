6aa1ad28575511429b600e22480df3bfd69ef69e
sudeepmi <sudeepm0599@gmail.com>
2022-11-10 22:16:31 +0545

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-     <Home />
+    <div className='main'>
+      <Navbar />
+      <Home />
     </div>
   )
 }
