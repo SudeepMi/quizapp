58f97f1305bcd4c6ebe441deceda2a00ebfd552e
sudeepmi <sudeepm0599@gmail.com>
2022-11-12 03:20:11 +0545

    signoff function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
 function Navbar() {
+  const navigate = useNavigate();
+  const islogged = localStorage.getItem("token") ? true : false;
   return (
-    <div className='navbar'>
-      <button>Sign off</button>
+    <div className="navbar">
+      {!islogged ? (
+        <button onClick={() => navigate("/signin")}>Sign In</button>
+      ) :
+      (
+        <button onClick={() => localStorage.clear()}>Sign Off</button>
+      ) 
+      }
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
