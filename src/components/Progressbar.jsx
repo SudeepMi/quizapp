9897beb1726dac57c843e0d2f7e11222fad253c2
surajgaire14 <eriag321@gmail.com>
2022-11-12 16:43:29 +0545

    progress bar skeleton added

---
+++ src/components/Progressbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Progressbar = () => {
+  return (
+    <div className="progressbar">
+      <div className="rapidfire"><p>Rapid Fire</p></div>
+      <div className="optionbased"><p>Question Based</p></div>
+      <div className="questionare"><p>Questionare</p></div>
+      <div className="code_competition"><p>Code Competition</p></div>
+    </div>
+  );
+};
+
+export default Progressbar;
