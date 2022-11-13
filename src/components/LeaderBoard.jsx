1d589c3ac64cf3911fc15759a7eb561fae752b48
surajgaire14 <eriag321@gmail.com>
2022-11-13 22:37:07 +0545

    leaderboard and progress updated

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
 function LeaderBoard() {
+  const leaders = [
+    { team: "Team A", attemted: "20", points: 10 },
+    { team: "Team B", attemted: "10", points: 10 },
+  ];
+
   return (
     <div className="leaderboards">
-      <div className="profile__box">
-        <img src={`https://robohash.org/1`} />
-        <div className="team">
-          <h3>Team A</h3>
-          <div className="rank">
-            <p>Attempted 20</p>
-            <p>Team Rank:- 1st</p>
+      {leaders.map((leader, i) => {
+        return (
+          <div className="profile__box">
+            <img src={`https://robohash.org/1`} />
+            <div className="team">
+              <h3>{leader.team}</h3>
+              <div className="rank">
+                <p>Attempted: {leader.attemted}</p>
+                <p>points: {leader.points} </p>
+              </div>
+            </div>
+            <p className="position">1st</p>
           </div>
-        </div>
-        <p className="position">1st</p>
-      </div>
+        );
+      })}
     </div>
   );
 }
