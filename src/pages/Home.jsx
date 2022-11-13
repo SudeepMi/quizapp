781fd47ee8b6f29c81922f4be208482f353430f4
sudeepmi <sudeepm0599@gmail.com>
2022-11-13 12:59:14 +0545

    real time question slide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,28 +3,15 @@ import LeaderBoard from '../components/LeaderBoard'
 import Questions from '../components/Questions'
 import UserProfile from '../components/UserProfile'
 import './home.css'
-import Pusher from "pusher-js"
 import Progressbar from '../components/Progressbar'
 import Answer from '../components/Answer'
 
 function Home() {
-  const next = () =>{
-    // Enable pusher logging - don't include this in production
-    // Pusher.logToConsole = true;
-    const pusher = new Pusher('2142cda6d39765cba2a9', {
-        cluster: 'ap2'
-    });
-
-    const channel = pusher.subscribe('main');
-    channel.bind('current', function (data) {
-        alert(JSON.stringify(data));
-    });
-  }
 
   return (
     <div className="home_wrapper">
      <UserProfile/>
-     <Questions next={next} />
+     <Questions mode={"optionBased"}/>
      <LeaderBoard />
      <Progressbar />
      <Answer />
