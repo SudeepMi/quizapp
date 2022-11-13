6e4104b3783156c0076e9f151b32b68c926a54cd
sudeepmi <sudeepm0599@gmail.com>
2022-11-13 13:29:12 +0545

    authorize to admin

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,45 +1,62 @@
-import React,{lazy, useEffect} from "react";
-import Pusher from "pusher-js"
+import React, { lazy, useEffect } from "react";
+import Pusher from "pusher-js";
 import axios from "axios";
 
-const _options = ["A. ","B. ","C. ","D. "];
+const _options = ["A. ", "B. ", "C. ", "D. "];
 
-function Questions({mode,questionid}) {
-
-  const [question,setQuestion] = React.useState({})
-  const [current, setcurrent] = React.useState(localStorage.getItem("current") || 0);
-
-  const getQuestion = async () =>{
-    const _questions = (await import(`./../questions/${mode}.json`).then(res=> res.default))
-    if(_questions[current]){
-      setQuestion(_questions[current])
+function Questions({ mode, questionid }) {
+  const [question, setQuestion] = React.useState({});
+  const [current, setcurrent] = React.useState(
+    localStorage.getItem("current") || 0
+  );
+  const isLoggedIn = localStorage.getItem("token") ? true : false;
+  const getQuestion = async () => {
+    const _questions = await import(`./../questions/${mode}.json`).then(
+      (res) => res.default
+    );
+    if (_questions[current]) {
+      setQuestion(_questions[current]);
     }
-    if(_questions[questionid]){
-      setQuestion(_questions[questionid])
+    if (_questions[questionid]) {
+      setQuestion(_questions[questionid]);
     }
-  }
+  };
 
   useEffect(() => {
-    getQuestion(mode)
-  }, [mode,current,questionid])
-  
-  const next = () =>{
-    setcurrent(current+1)
-    axios.post("https://quizapp-server-production.up.railway.app/",{n:current+1}).then(res=>console.log(res))
-  }
+    getQuestion(mode);
+  }, [mode, current, questionid]);
+
+  const next = () => {
+    setcurrent(current + 1);
+    axios
+      .post("https://quizapp-server-production.up.railway.app/", {
+        n: current + 1,
+      })
+      .then((res) => console.log(res));
+  };
   return (
-    question &&
-    <div className="questions">
-      <h2>{question.question}</h2>
-      <div className="option_area">
-       {question.option && question.option.map((option,key)=><p key={key}>{_options[key]}{option}</p>)}
+    question && (
+      <div className="questions">
+        <h2>{question.question}</h2>
+        <div className="option_area">
+          {question.option &&
+            question.option.map((option, key) => (
+              <p key={key}>
+                {_options[key]}
+                {option}
+                {(question.answer==key+1 && isLoggedIn )&& <i className="ri-check-double-line"></i>}
+              </p>
+            ))}
+        </div>
+        {isLoggedIn && (
+          <div className="buttons">
+            <button>Reveal Answer</button>
+            <button onClick={() => next()}>Next</button>
+            <button>Pause Timer</button>
+          </div>
+        )}
       </div>
-      <div className="buttons">
-        <button>Reveal Answer</button>
-        <button onClick={() => next()}>Next</button>
-        <button>Pause Timer</button>
-      </div>
-    </div> 
+    )
   );
 }
 
