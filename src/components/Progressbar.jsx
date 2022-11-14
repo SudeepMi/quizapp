e02353784c795917a6771826c0c6c9b960427c00
surajgaire14 <eriag321@gmail.com>
2022-11-14 13:04:21 +0545

    team form added

--- src/components/Progressbar.jsx
+++ src/components/Progressbar.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-const Progressbar = () => {
-  const [clicked,setClicked] = React.useState(false)
-  console.log(clicked);
+const Progressbar = (id) => {
+  const [clicked,setClicked] = React.useState({})
+
+  console.log(clicked)
+
+  const changeColor = (id) => {
+      setClicked({...clicked,[id]:!clicked[id]})
+  }
+
   return (
     <div className={`progressbar ${clicked && "animation"}`}>
-      <div className={`rapidfire ${clicked && "animation"}`} onClick={() => setClicked(true)}><p>Rapid Fire</p></div>
-      <div className={`optionbased `} onClick={() => setClicked(true)}><p>Question Based</p></div>
-      <div className="questionare"><p>Questionare</p></div>
-      <div className="code_competition"><p>Code Competition</p></div>
+      <span className={`${clicked[1] && "animation"}`}></span>
+      <span className={`${clicked[2] && "animation1"}`}></span>
+      <span className={`${clicked[3] && "animation2"}`}></span>
+      <div className="rapidfire"><p>Rapid Fire</p></div>
+      <div className={`optionbased `} onClick={() => changeColor(1)}><p>Question Based</p></div>
+      <div className={`questionare`} onClick= {() => changeColor(2)}><p>Questionare</p></div>
+      <div className="code_competition" onClick={() => changeColor(3)}><p>Code Competition</p></div>
     </div>
   );
 };
